a4e6053637faed353efd965c869dc72d9d0b14ec
jayrajgb <jayrajgborate11@gmail.com>
2024-02-25 15:54:10 +0530

    Contents Added

--- wt-cp/src/Books.jsx
+++ wt-cp/src/Books.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Bookspage from './components/Bookspage'
 
 const Books = () => {
   return (
     <div>
         <Navbar/>
+        <Bookspage/>
     </div>
   )
 }
